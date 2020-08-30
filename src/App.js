73a46176155c4fb45b7d54ad97222f30601f1c23
iqbalfaisal <7faisaliqbal@gmail.com>
2020-08-30 09:33:01 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+
+      <Button
+        onPress={() => navigation.navigate('Tweet')}
+        title="Go to Tweet"
+      />
+    </View>
+  );
+}
+
+function TweetScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Tweet</Text>
+    </View>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeNavigator2() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Tweet" component={TweetScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeNavigator2} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function NotificationsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function NomeNavigation() {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={TabNavigator} />
+      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+function LoginScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Login Screen</Text>
+      <Button
+        onPress={() => navigation.navigate('NomeNavigation')}
+        title="Go to Nome"
+      />
+    </View>
+  );
+}
+
+function RegisterScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Rgister Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+
+        <Stack.Screen name="NomeNavigation" component={NomeNavigation} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
